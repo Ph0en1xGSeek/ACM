dab358c6d26bfb2d627595ed6cb5fbf91fa15726
Ph0en1x <54panguosheng@gmail.com>
2018-08-06 22:05:56 +0800

    2018.8.6

---
+++ LeetCode/43.py
@@ -0,0 +1,11 @@
+class Solution:
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        n1 = int(num1)
+        n2 = int(num2)
+        res = n1 * n2
+        return str(res)
