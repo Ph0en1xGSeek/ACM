43e5a83f53c0994fe6135ab6e5b5b5a6d6bc9f98
Ph0en1xGSeek <54panguosheng@gmail.com>
2020-09-13 19:57:54 +0800

    2020.9.13

--- LeetCode/4.py
+++ LeetCode/4.py
@@ -39,4 +39,43 @@ class Solution(object):
                 return arr[(len1 + len2) // 2]
                 
                 
-                
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int size1= nums1.size();
+        int size2 = nums2.size();
+        int tot = size1 + size2;
+        if(tot & 1) {
+            return findK(nums1, nums2, tot / 2 + 1);
+        }else {
+            return (findK(nums1, nums2, tot / 2) + findK(nums1, nums2, tot / 2 + 1)) / 2.0;
+        }
+    }
+
+    int findK(vector<int> &nums1, vector<int> &nums2, int k) {
+        int size1 = nums1.size();
+        int size2 = nums2.size();
+        int index1 = 0, index2 = 0;
+
+        while(true) {
+            if(index1 == size1) {
+                return nums2[index2 + k - 1];
+            }
+            if(index2 == size2) {
+                return nums1[index1 + k - 1];
+            }
+            if(k == 1) {
+                return min(nums1[index1], nums2[index2]);
+            }
+            int new_index1 = min(size1 - 1, index1 + k / 2 - 1);
+            int new_index2 = min(size2 - 1, index2 + k / 2 - 1);
+            if(nums1[new_index1] < nums2[new_index2]) {
+                k -= new_index1 - index1 + 1;
+                index1 = new_index1 + 1;
+            }else {
+                k -= new_index2 - index2 + 1;
+                index2 = new_index2 + 1;
+            }
+        }
+    }
+};
