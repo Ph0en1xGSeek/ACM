dd96231bff2cb67b0c43a959157d5d11f4cd2130
Ph0en1xGSeek <54panguosheng@gmail.com>
2021-04-02 21:25:14 +0800

    2021.4.2

--- LeetCode/808.cpp
+++ LeetCode/808.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     double soupServings(int N) {
     	N = (N + 24) / 25;
-	if(N >= 500) {
-		return 1.0;
-	}
-	vector<vector<double>> memo(N + 1, vector<double>(N + 1, 0));
-	for(int s = 0; s <= 2 * N; ++s) {
-		for(int i = 0; i <= N; ++i) {
-			int j = s - i;
-			if(j < 0 || j > N) {
-				continue;
-			}
-			double ans = 0;
-			if(i == 0 && j == 0) {
-				ans = 0.5;
-			}else if(i == 0) {
-				ans = 1.0;
-			}
-			if(i > 0 && j > 0) {
-				ans = 0.25 * (memo[max(i - 4, 0)][j] + memo[max(i - 3, 0)][max(j - 1, 0)] + 
-                                memo[max(i - 2, 0)][max(j - 2, 0)] + memo[max(i - 1, 0)][max(j - 3, 0)]);
-			}
-			memo[i][j] = ans;
+		if(N >= 500) {
+			return 1.0;
 		}
-	}
-	return memo[N][N];
+		vector<vector<double>> memo(N + 1, vector<double>(N + 1, 0));
+		for(int s = 0; s <= 2 * N; ++s) {
+			for(int i = 0; i <= N; ++i) {
+				int j = s - i;
+				if(j < 0 || j > N) {
+					continue;
+				}
+				double ans = 0;
+				if(i == 0 && j == 0) {
+					ans = 0.5;
+				}else if(i == 0) {
+					ans = 1.0;
+				}
+				if(i > 0 && j > 0) {
+					ans = 0.25 * (memo[max(i - 4, 0)][j] + memo[max(i - 3, 0)][max(j - 1, 0)] + 
+									memo[max(i - 2, 0)][max(j - 2, 0)] + memo[max(i - 1, 0)][max(j - 3, 0)]);
+				}
+				memo[i][j] = ans;
+			}
+		}
+		return memo[N][N];
     }
 };
